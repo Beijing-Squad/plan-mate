175b563997927a8988484e6191c68fd1d2e0e1b0
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-28 14:46:25 +0300

    create states data source abstraction

--- src/main/kotlin/data/repository/dataSourceAbstraction/StatesDataSource.kt
+++ src/main/kotlin/data/repository/dataSourceAbstraction/StatesDataSource.kt
@@ -1,2 +1,19 @@
 package data.repository.dataSourceAbstraction
 
+import logic.entities.State
+
+interface StatesDataSource{
+
+    fun getAllStates(): List<State>
+
+    fun getStatesByProjectId(projectId: String): List<State>
+
+    fun getStateById(stateId: String): State
+
+    fun addState(state: State)
+
+    fun updateState(state: State)
+
+    fun deleteState(state: State)
+
+}
