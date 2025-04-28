322c20203117912b3d54470cd531fa98ecf6e2ce
AdelTamer <adeltammer@gmail.com>
2025-04-28 19:45:13 +0300

    create manage authentication use case

--- src/main/kotlin/logic/useCases/ManageAuthenticationUseCase.kt
+++ src/main/kotlin/logic/useCases/ManageAuthenticationUseCase.kt
@@ -1,2 +1,9 @@
 package logic.useCases
 
+import logic.repository.AuthenticationRepository
+
+class ManageAuthenticationUseCase(
+    private val authenticationRepository: AuthenticationRepository
+) {
+
+}
