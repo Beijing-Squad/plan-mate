a434c0c0ebcf0e5918f16a56ea379145545abf97
AdelTamer <adeltammer@gmail.com>
2025-04-28 19:45:34 +0300

    add custom exceptions

--- src/main/kotlin/utils/PlanMateExceptions.kt
+++ src/main/kotlin/utils/PlanMateExceptions.kt
@@ -1,9 +1,24 @@
 package utils
 
 open class PlanMateException(message: String) : Exception(message)
-class AuthenticationException(message: String) : PlanMateException(message)
-class ProjectException(message: String) : PlanMateException(message)
-class TaskException(message: String) : PlanMateException(message)
+
+open class AuthenticationException(message: String) : PlanMateException(message)
+class UserNotFoundException(message: String) : AuthenticationException(message)
+class InvalidLoginException(message: String) : AuthenticationException(message)
+
+open class ProjectException(message: String) : PlanMateException(message)
+class ProjectAlreadyExistsException(message: String) : ProjectException(message)
+class ProjectNotFoundException(message: String) : ProjectException(message)
+
+open class TaskException(message: String) : PlanMateException(message)
+class TaskAlreadyExistsException(message: String) : TaskException(message)
+class TaskNotFoundException(message: String) : TaskException(message)
+
 class StateException(message: String) : PlanMateException(message)
-class ValidationException(message: String) : PlanMateException(message)
+
+open class ValidationException(message: String) : PlanMateException(message)
+class InvalidInputException(message: String) : ValidationException(message)
+class InvalidUserNameException(message: String) : ValidationException(message)
+class InvalidPasswordException(message: String) : ValidationException(message)
+
 class DataAccessException(message: String) : PlanMateException(message)
