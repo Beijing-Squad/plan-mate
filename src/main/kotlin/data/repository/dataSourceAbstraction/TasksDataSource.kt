f247f73901a3e7ecd20061c276e4fd5e041e283f
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-28 14:46:43 +0300

    create tasks data source abstraction

--- src/main/kotlin/data/repository/dataSourceAbstraction/TasksDataSource.kt
+++ src/main/kotlin/data/repository/dataSourceAbstraction/TasksDataSource.kt
@@ -1,2 +1,18 @@
 package data.repository.dataSourceAbstraction
 
+import logic.entities.Task
+
+interface TasksDataSource {
+
+    fun getAllTasks(): List<Task>
+
+    fun getTaskById(taskId: String): Task
+
+    fun addTask(task: Task)
+
+    fun deleteTask(taskId: String)
+
+    fun updateTask(taskId: String): Task
+
+}
+
