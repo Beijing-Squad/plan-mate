d5c2d7ac77b3a963662460cab6c506f5dac60934
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-28 14:46:53 +0300

    create user data source abstraction

--- src/main/kotlin/data/repository/dataSourceAbstraction/UserDataSource.kt
+++ src/main/kotlin/data/repository/dataSourceAbstraction/UserDataSource.kt
@@ -1,2 +1,14 @@
 package data.repository.dataSourceAbstraction
 
+import logic.entities.User
+
+interface UserDataSource{
+
+    fun getAllUsers(): List<User>
+
+    fun getUserByUserId(userId: String): User
+
+    fun addUser(user: User)
+
+    fun updateUser(userId: String): User
+}
