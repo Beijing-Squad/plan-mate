f63ef8e2c18985bb035416a248e63073216c352a
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-28 14:46:13 +0300

    create project data source abstraction

--- src/main/kotlin/data/repository/dataSourceAbstraction/ProjectDataSource.kt
+++ src/main/kotlin/data/repository/dataSourceAbstraction/ProjectDataSource.kt
@@ -1,2 +1,18 @@
 package data.repository.dataSourceAbstraction
 
+import logic.entities.Project
+
+interface ProjectDataSource{
+
+    fun getAllProjects(): List<Project>
+
+    fun getProjectById(projectId: String): Project
+
+    fun addTask(project: Project)
+
+    fun deleteTask(projectId: String)
+
+    fun updateTask(projectId: String): Project
+
+}
+
