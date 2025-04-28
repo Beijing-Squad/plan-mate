608ce247636f65709d186f104540ef83e396b6a4
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-28 10:42:09 +0300

    create task model

--- src/main/kotlin/logic/entities/Task.kt
+++ src/main/kotlin/logic/entities/Task.kt
@@ -1,2 +1,17 @@
 package logic.entities
 
+import kotlinx.datetime.LocalDate
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+data class Task(
+    val id: Uuid = Uuid.random(),
+    val projectId: String,
+    val title: String,
+    val description: String,
+    val createdBy: String,
+    val stateId: String,
+    val createdAt: LocalDate,
+    val updatedAt: LocalDate
+)
