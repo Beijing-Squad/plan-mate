478c8272e859bf06c501e7fb47f88717400132d0
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-28 10:42:27 +0300

    create project model

--- src/main/kotlin/logic/entities/Project.kt
+++ src/main/kotlin/logic/entities/Project.kt
@@ -1,2 +1,15 @@
 package logic.entities
 
+import kotlinx.datetime.LocalDate
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+data class Project(
+    val id: Uuid = Uuid.random(),
+    val name: String,
+    val description: String,
+    val createdBy: String,
+    val createdAt: LocalDate,
+    val updatedAt: LocalDate
+)
