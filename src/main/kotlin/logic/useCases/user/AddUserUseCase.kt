674c46663c1ea7e873224e40bfcc18e2efc9141b
AdelTamer <adeltammer@gmail.com>
2025-04-28 22:51:38 +0300

    remove unused imports

--- src/main/kotlin/logic/useCases/user/AddUserUseCase.kt
+++ src/main/kotlin/logic/useCases/user/AddUserUseCase.kt
@@ -1,8 +1,6 @@
 package logic.useCases.user
 
-import logic.entities.User
 import logic.repository.UserRepository
-import kotlin.uuid.ExperimentalUuidApi
 
 class AddUserUseCase(
     private val userRepository: UserRepository
