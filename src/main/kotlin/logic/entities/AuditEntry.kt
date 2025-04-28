1da43b7e316311e5ccc3609d3ab857254327b0c2
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-28 10:42:36 +0300

    create audit model

--- src/main/kotlin/logic/entities/AuditEntry.kt
+++ src/main/kotlin/logic/entities/AuditEntry.kt
@@ -1,2 +1,18 @@
 package logic.entities
 
+import kotlinx.datetime.LocalDate
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+data class AuditEntry(
+    val id: Uuid = Uuid.random(),
+    val userRole: UserRole,
+    val userName: String,
+    val entityId: String,
+    val entityType: EntityType,
+    val action: ActionType,
+    val oldState: String?,
+    val newState: String?,
+    val timeStamp: LocalDate
+)
