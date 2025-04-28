795d3f268df6317239f18982116a0d3e45158c15
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-28 14:46:01 +0300

    create authentication data source abstraction

--- src/main/kotlin/data/repository/dataSourceAbstraction/AuthenticationDataSource.kt
+++ src/main/kotlin/data/repository/dataSourceAbstraction/AuthenticationDataSource.kt
@@ -1,2 +1,12 @@
 package data.repository.dataSourceAbstraction
 
+import logic.entities.User
+import logic.entities.UserRole
+
+interface AuthenticationDataSource{
+
+    fun register(userName: String, password: String, userRole: UserRole): User
+
+    fun save(user: User)
+
+}
