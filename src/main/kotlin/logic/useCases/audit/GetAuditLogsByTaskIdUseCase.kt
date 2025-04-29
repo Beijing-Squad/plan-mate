a972fe063f5893a10559722eecce22bc1ca758e7
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-04-29 22:08:49 +0300

    add function getAuditLogsByTaskId to call in test cases

--- src/main/kotlin/logic/useCases/audit/GetAuditLogsByTaskIdUseCase.kt
+++ src/main/kotlin/logic/useCases/audit/GetAuditLogsByTaskIdUseCase.kt
@@ -1,8 +1,15 @@
 package logic.useCases.audit
 
+import logic.entities.Audit
 import logic.repository.AuditRepository
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 class GetAuditLogsByTaskIdUseCase(
     private val auditRepository: AuditRepository
 ) {
+    @OptIn(ExperimentalUuidApi::class)
+    fun getAuditLogsByTaskId(taskId: Uuid): List<Audit> {
+        return emptyList()
+    }
 }
