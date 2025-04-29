c677af7acd3fe8d1f73caa7597958ab7c426c098
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-29 17:08:49 +0300

    add csv parser module

--- src/main/kotlin/di/KoinModules.kt
+++ src/main/kotlin/di/KoinModules.kt
@@ -1,2 +1,12 @@
 package di
 
+import data.parser.*
+import org.koin.dsl.module
+
+val csvParsersModule = module {
+    single { UserCsvParser() }
+    single { TaskCsvParser() }
+    single { StateCsvParser() }
+    single { AuditCsvParser() }
+    single { ProjectCsvParser() }
+}
