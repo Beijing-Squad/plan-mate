f32604cc67cef7316deabe7f04bb5d011b7cf448
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-04-29 22:13:40 +0300

    refactor function getAuditLogsByTaskId to call in test cases correctly

--- src/main/kotlin/logic/useCases/audit/GetAuditLogsByTaskIdUseCase.kt
+++ src/main/kotlin/logic/useCases/audit/GetAuditLogsByTaskIdUseCase.kt
@@ -2,14 +2,12 @@ package logic.useCases.audit
 
 import logic.entities.Audit
 import logic.repository.AuditRepository
-import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
 
 class GetAuditLogsByTaskIdUseCase(
     private val auditRepository: AuditRepository
 ) {
-    @OptIn(ExperimentalUuidApi::class)
-    fun getAuditLogsByTaskId(taskId: Uuid): List<Audit> {
+
+    fun getAuditLogsByTaskId(taskId: String): List<Audit> {
         return emptyList()
     }
 }
