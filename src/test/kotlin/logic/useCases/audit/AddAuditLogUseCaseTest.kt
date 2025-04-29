d69fb91de315c91a1c6f68200f8f73c335cc167e
AdelTamer <adeltammer@gmail.com>
2025-04-30 00:40:29 +0300

    write add audit log test case

--- src/test/kotlin/logic/useCases/audit/AddAuditLogUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/audit/AddAuditLogUseCaseTest.kt
@@ -1,7 +1,40 @@
 package logic.useCases.audit
 
-import org.junit.jupiter.api.Assertions.*
+import fake.createAudit
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.datetime.LocalDate
+import logic.entities.ActionType
+import logic.entities.EntityType
+import logic.entities.UserRole
+import logic.repository.AuditRepository
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class AddAuditLogUseCaseTest {
+    private lateinit var auditRepository: AuditRepository
+    private lateinit var addAuditLogUseCase: AddAuditLogUseCase
+
+    @BeforeEach
+    fun setUp() {
+        auditRepository = mockk(relaxed = true)
+        addAuditLogUseCase = AddAuditLogUseCase(auditRepository)
+    }
+
+    @Test
+    fun `should add audit log when called`() {
+        // Given
+        val auditLog = createAudit(
+            userRole = UserRole.ADMIN,
+            userName = "Adel",
+            action = ActionType.CREATE,
+            entityType = EntityType.PROJECT,
+            entityId = "PROJECT-001",
+            timeStamp = LocalDate(2025, 4, 30)
+        )
+
+        // When && Then
+        verify { addAuditLogUseCase.addAuditLog(auditLog) }
+    }
 
 }
