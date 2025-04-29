862b003d74adf49899cec0d5f300c6c7b42de647
AdelTamer <adeltammer@gmail.com>
2025-04-29 19:32:51 +0300

    create get audit logs by project id function

--- src/main/kotlin/logic/useCases/audit/GetAuditLogsByProjectIdUseCase.kt
+++ src/main/kotlin/logic/useCases/audit/GetAuditLogsByProjectIdUseCase.kt
@@ -1,8 +1,12 @@
 package logic.useCases.audit
 
+import logic.entities.Audit
 import logic.repository.AuditRepository
 
 class GetAuditLogsByProjectIdUseCase(
     private val auditRepository: AuditRepository
 ) {
+    fun getAuditLogsByProjectId(projectId: String): List<Audit>{
+        return emptyList()
+    }
 }
