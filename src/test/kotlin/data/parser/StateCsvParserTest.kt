0971162b318a996bfabe9055fa37c7a178a8452c
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-29 17:06:37 +0300

    add unit tests for state parser

---
+++ src/test/kotlin/data/parser/StateCsvParserTest.kt
@@ -0,0 +1,42 @@
+package data.parser
+
+import com.google.common.truth.Truth.assertThat
+import kotlinx.datetime.LocalDate
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+
+class StateCsvParserTest {
+
+    private lateinit var parser: StateCsvParser
+
+    @BeforeEach
+    fun setup(){
+        parser = StateCsvParser()
+    }
+
+    @Test
+    fun `header returns correct CSV header`() {
+        // When
+        val header = parser.header()
+
+        // Then
+        assertThat(header).isEqualTo("id,name,projectId")
+    }
+
+    @Test
+    fun `deserializer returns correct Audit object`() {
+        // Given
+        val line = "252,TODO,123e4567-e89b-12d3-a456-426614174000"
+
+        // When
+        val result = parser.deserializer(line)
+
+        // Then
+        assertThat(result.id).isEqualTo("252")
+        assertThat(result.name).isEqualTo("TODO")
+        assertThat(result.projectId).isEqualTo("123e4567-e89b-12d3-a456-426614174000")
+    }
+
+}
