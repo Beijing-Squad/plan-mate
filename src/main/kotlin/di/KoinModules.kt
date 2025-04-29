87f7f581cac73ef9518de30687088af637d1404f
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-29 19:39:10 +0300

    refactor app module

--- src/main/kotlin/di/KoinModules.kt
+++ src/main/kotlin/di/KoinModules.kt
@@ -1,12 +1,10 @@
 package di
 
-import data.parser.*
-import org.koin.dsl.module
-
-val csvParsersModule = module {
-    single { UserCsvParser() }
-    single { TaskCsvParser() }
-    single { StateCsvParser() }
-    single { AuditCsvParser() }
-    single { ProjectCsvParser() }
-}
+val appModule = listOf(
+    fileModule,
+    csvModule,
+    parsersModule,
+    dataSourceModule,
+    repositoryModule,
+    useCaseModule
+)
