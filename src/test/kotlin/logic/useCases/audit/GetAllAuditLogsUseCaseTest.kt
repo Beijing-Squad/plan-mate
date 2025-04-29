1d955f3edb1c74cca08d276c1c5d9211064f9aa5
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-04-29 17:45:48 +0300

    add test setup for GetAllAuditLogsUseCaseTest

--- src/test/kotlin/logic/useCases/audit/GetAllAuditLogsUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/audit/GetAllAuditLogsUseCaseTest.kt
@@ -1,7 +1,22 @@
 package logic.useCases.audit
 
+import data.repository.AuditRepositoryImpl
+import data.repository.dataSource.AuditDataSource
+import io.mockk.mockk
+import logic.entities.Audit
+import logic.repository.AuditRepository
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 
 class GetAllAuditLogsUseCaseTest {
+
+    lateinit var getAllAuditLogs: GetAllAuditLogsUseCase
+    lateinit var auditRepository: AuditRepository
+
+    @BeforeEach
+    fun setUp() {
+        auditRepository = AuditRepositoryImpl(mockk(relaxed = true))
+        getAllAuditLogs = GetAllAuditLogsUseCase(auditRepository)
+    }
 
 }
