2a0d5e4dc0a12bac09af4d24c77eb1572e214ffd
AdelTamer <adeltammer@gmail.com>
2025-04-29 18:29:55 +0300

    add setup for get audit logs by project id use case test

--- src/test/kotlin/logic/useCases/audit/GetAuditLogsByProjectIdUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/audit/GetAuditLogsByProjectIdUseCaseTest.kt
@@ -1,7 +1,19 @@
 package logic.useCases.audit
 
-import org.junit.jupiter.api.Assertions.*
+import io.mockk.mockk
+import logic.repository.AuditRepository
+import org.junit.jupiter.api.BeforeEach
 
 class GetAuditLogsByProjectIdUseCaseTest {
+
+    private lateinit var auditRepository: AuditRepository
+    private lateinit var getAuditLogsByProjectIdUseCase: GetAuditLogsByProjectIdUseCase
+
+    @BeforeEach
+    fun setUp() {
+        auditRepository = mockk()
+        getAuditLogsByProjectIdUseCase = GetAuditLogsByProjectIdUseCase(auditRepository)
+    }
+
 
 }
