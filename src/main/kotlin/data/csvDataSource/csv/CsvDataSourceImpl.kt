17a62bf25699fb888d750320a91f89c3ed63b32d
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2025-04-29 17:05:28 +0300

    implement csv data source for parsing

---
+++ src/main/kotlin/data/csvDataSource/csv/CsvDataSourceImpl.kt
@@ -0,0 +1,43 @@
+package data.csvDataSource.csv
+
+import logic.entities.exceptions.CsvReadException
+import logic.entities.exceptions.CsvWriteException
+
+class CsvDataSourceImpl<T>(
+    private var reader: CsvReader,
+    private var writer: CsvWriter,
+    private val parser: CsvParser<T>,
+) {
+    fun loadAll(): List<T> {
+        return try {
+            val lines = reader.readCsv()
+            if (lines.size <= 1) return emptyList()
+
+            lines.drop(1).map(parser::deserializer)
+        } catch (e: Exception) {
+            throw CsvReadException("Error reading CSV file: ${e.message}")
+        }
+    }
+
+    fun append(item: T) {
+        try {
+            val rawLines = reader.readCsv()
+            if (rawLines.isEmpty()) {
+                writer.appendLine(parser.header())
+            }
+
+            writer.appendLine(parser.serializer(item))
+        } catch (e: Exception) {
+            throw CsvWriteException("Error writing to CSV file: ${e.message}")
+        }
+    }
+
+    fun update(items: List<T>) {
+        try {
+            val serializedLines = items.map(parser::serializer)
+            writer.updateLines(listOf(parser.header()) + serializedLines)
+        } catch (e: Exception) {
+            throw CsvWriteException("Error saving to CSV file: ${e.message}")
+        }
+    }
+}
---
+++ src/test/kotlin/data/parser/UserCsvParserTest.kt
@@ -0,0 +1,81 @@
+package data.parser
+
+import com.google.common.truth.Truth.assertThat
+import fake.createTask
+import fake.createUser
+import kotlinx.datetime.LocalDate
+import logic.entities.UserRole
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+class UserCsvParserTest {
+
+    private lateinit var parser: UserCsvParser
+
+    @BeforeEach
+    fun setup(){
+        parser = UserCsvParser()
+    }
+
+    @Test
+    fun `header returns correct CSV header`() {
+        // When
+        val header = parser.header()
+
+        // Then
+        assertThat(header).isEqualTo("id,userName,password,role")
+    }
+
+    @Test
+    fun `serializer returns correct CSV line`() {
+        // Given
+        val fixedId = Uuid.parse("123e4567-e89b-12d3-a456-426614174000")
+        val task = createUser(
+        ).copy(id = fixedId)
+
+        // When
+        val result = parser.serializer(task)
+
+        // Then
+        assertThat(result).isEqualTo("123e4567-e89b-12d3-a456-426614174000," +
+                "defaultUser," +
+                "defaultPassword," +
+                "MATE")
+    }
+
+    @Test
+    fun `deserializer returns correct Audit object`() {
+        // Given
+        val line = "123e4567-e89b-12d3-a456-426614174000," +
+                   "Youssef," +
+                   "Youssef10125," +
+                   "MATE,"
+
+
+        // When
+        val result = parser.deserializer(line)
+
+        // Then
+        assertThat(result.id.toString()).isEqualTo("123e4567-e89b-12d3-a456-426614174000")
+        assertThat(result.userName).isEqualTo("Youssef")
+        assertThat(result.password).isEqualTo("Youssef10125")
+        assertThat(result.role).isEqualTo(UserRole.MATE)
+    }
+
+    @Test
+    fun `deserializer throws IllegalArgumentException for bad UUID`() {
+        //Given
+        val badLine = "not-a-uuid,Type,ID,ACTION,user,timestamp"
+
+        //When && Then
+        assertThrows<IllegalArgumentException> {
+            parser.deserializer(badLine)
+        }
+    }
+
+
+}
