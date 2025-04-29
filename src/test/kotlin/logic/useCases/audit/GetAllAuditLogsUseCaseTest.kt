824a9da4c4e9ca2ac1f57e9b5ad7ba5da2a0f559
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-04-29 18:38:41 +0300

    add a test case for returning all audit logs when called

--- src/test/kotlin/logic/useCases/audit/GetAllAuditLogsUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/audit/GetAllAuditLogsUseCaseTest.kt
@@ -1,9 +1,18 @@
 package logic.useCases.audit
 
+import com.google.common.truth.Truth.assertThat
 import data.repository.AuditRepositoryImpl
+import fake.createAudit
+import io.mockk.every
 import io.mockk.mockk
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import logic.entities.EntityType
+import logic.entities.UserRole
 import logic.repository.AuditRepository
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
 
 class GetAllAuditLogsUseCaseTest {
 
@@ -15,4 +24,30 @@ class GetAllAuditLogsUseCaseTest {
         auditRepository = AuditRepositoryImpl(mockk(relaxed = true))
         getAllAuditLogs = GetAllAuditLogsUseCase(auditRepository)
     }
+
+    @Test
+    fun `should return all audit logs when called`() {
+        // Given
+        every { auditRepository.getAllAuditLogs() } returns listOf(
+            createAudit(
+                userRole = UserRole.ADMIN,
+                userName = "Admin",
+                entityType = EntityType.PROJECT,
+                timeStamp = LocalDate(2023, 1, 1)
+            ),
+            createAudit(
+                userRole = UserRole.MATE,
+                userName = "MATE",
+                entityType = EntityType.TASK,
+                timeStamp = LocalDate(2023, 1, 12)
+            )
+        )
+
+        // When
+        val result = auditRepository.getAllAuditLogs()
+
+        // Then
+        assertThat(result.size).isEqualTo(2)
+    }
+
 }
