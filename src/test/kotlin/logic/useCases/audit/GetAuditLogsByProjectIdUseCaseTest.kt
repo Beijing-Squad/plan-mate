19c0c7231414704ee27a342fcfd7bd2d7305a2c3
AdelTamer <adeltammer@gmail.com>
2025-04-29 19:34:18 +0300

    add test case when given project id is valid

--- src/test/kotlin/logic/useCases/audit/GetAuditLogsByProjectIdUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/audit/GetAuditLogsByProjectIdUseCaseTest.kt
@@ -1,8 +1,16 @@
 package logic.useCases.audit
 
+import com.google.common.truth.Truth.assertThat
+import fake.createAudit
+import io.mockk.every
 import io.mockk.mockk
+import kotlinx.datetime.LocalDate
+import logic.entities.ActionType
+import logic.entities.EntityType
+import logic.entities.UserRole
 import logic.repository.AuditRepository
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class GetAuditLogsByProjectIdUseCaseTest {
 
@@ -15,5 +23,34 @@ class GetAuditLogsByProjectIdUseCaseTest {
         getAuditLogsByProjectIdUseCase = GetAuditLogsByProjectIdUseCase(auditRepository)
     }
 
+    @Test
+    fun `should return list of audit logs when given project id is valid`() {
+        // Given
+        val givenId = "PROJECT-001"
+        every { auditRepository.getAllAuditLogs() } returns listOf(
+            createAudit(
+                userRole = UserRole.ADMIN,
+                userName = "Adel",
+                action = ActionType.CREATE,
+                entityType = EntityType.PROJECT,
+                entityId = givenId,
+                timeStamp = LocalDate(2025, 4, 29)
+            ),
+            createAudit(
+                userRole = UserRole.ADMIN,
+                userName = "Adel",
+                action = ActionType.UPDATE,
+                entityType = EntityType.PROJECT,
+                entityId = givenId,
+                timeStamp = LocalDate(2025, 4, 29)
+            )
+        )
+
+        // When
+        val result = getAuditLogsByProjectIdUseCase.getAuditLogsByProjectId(givenId)
+
+        // Then
+        assertThat(result.size).isEqualTo(2)
+    }
 
 }
