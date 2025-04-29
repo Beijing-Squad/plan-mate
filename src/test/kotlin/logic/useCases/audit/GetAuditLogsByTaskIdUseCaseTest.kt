4a1a5b4695c801233ba8221cc4ffc932aad03fec
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-04-29 22:00:05 +0300

    add test case setup for GetAuditLogsByTaskIdUseCaseTest

--- src/test/kotlin/logic/useCases/audit/GetAuditLogsByTaskIdUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/audit/GetAuditLogsByTaskIdUseCaseTest.kt
@@ -1,7 +1,20 @@
 package logic.useCases.audit
 
+import data.repository.AuditRepositoryImpl
+import io.mockk.mockk
+import logic.repository.AuditRepository
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 
 class GetAuditLogsByTaskIdUseCaseTest {
+
+    private lateinit var getAuditLogsByTaskIdUseCase: GetAuditLogsByTaskIdUseCase
+    private lateinit var auditRepository: AuditRepository
+
+    @BeforeEach
+    fun setUp() {
+        auditRepository = AuditRepositoryImpl(mockk(relaxed = true))
+        getAuditLogsByTaskIdUseCase = GetAuditLogsByTaskIdUseCase(auditRepository)
+    }
 
 }
