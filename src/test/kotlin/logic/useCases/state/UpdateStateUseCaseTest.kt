fcc1c7bf65b14400ee9652af769d4052efc7ef3a
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-04-30 15:25:44 +0300

    add test case for update state use case

--- src/test/kotlin/logic/useCases/state/UpdateStateUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/state/UpdateStateUseCaseTest.kt
@@ -1,7 +1,44 @@
 package logic.useCases.state
 
-import org.junit.jupiter.api.Assertions.*
+import fake.createProject
+import fake.createState
+import io.mockk.mockk
+import logic.repository.StatesRepository
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.uuid.ExperimentalUuidApi
 
 class UpdateStateUseCaseTest {
+    private lateinit var statesRepository: StatesRepository
+    private lateinit var updateStateUseCase: UpdateStateUseCase
+
+    @BeforeEach
+    fun setup() {
+        statesRepository = mockk()
+        updateStateUseCase = UpdateStateUseCase(statesRepository)
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    @Test
+    fun `update new state test`() {
+        //Given
+        val project = createProject(
+            name = "PlanMate Core Features",
+            createdBy = "adminUser01"
+        )
+        val newState = createState(
+            id = "123",
+            name = "Done",
+            projectId = project.id.toString()
+        )
+
+        // when
+//        every { statesRepository.updateState(newState) } returns true
+        val result = updateStateUseCase.updateState(newState)
+
+        //Then
+        assertEquals(true, result)
+    }
 
 }
