5e86bb4720cb994d85fc8906015385aa6732da26
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-04-30 15:23:05 +0300

    create add state fun use case for testing

--- src/main/kotlin/logic/useCases/state/AddStateUseCase.kt
+++ src/main/kotlin/logic/useCases/state/AddStateUseCase.kt
@@ -1,8 +1,12 @@
 package logic.useCases.state
 
+import logic.entities.State
 import logic.repository.StatesRepository
 
 class AddStateUseCase(
     private val statesRepository: StatesRepository
 ) {
+    fun addState(state: State): Boolean {
+        return false
+    }
 }
