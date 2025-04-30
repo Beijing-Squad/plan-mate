c18613eb41f21863114a901cc498c87847ddcf56
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-04-30 16:58:18 +0300

    add logic implementation for GetAuditLogsByTaskIdUseCase to pass the test cases

--- src/main/kotlin/logic/useCases/audit/GetAuditLogsByTaskIdUseCase.kt
+++ src/main/kotlin/logic/useCases/audit/GetAuditLogsByTaskIdUseCase.kt
@@ -1,6 +1,8 @@
 package logic.useCases.audit
 
 import logic.entities.Audit
+import logic.entities.EntityType
+import logic.entities.exceptions.InvalidInputException
 import logic.repository.AuditRepository
 
 class GetAuditLogsByTaskIdUseCase(
@@ -8,6 +10,15 @@ class GetAuditLogsByTaskIdUseCase(
 ) {
 
     fun getAuditLogsByTaskId(taskId: String): List<Audit> {
-        return emptyList()
+        validateTaskId(taskId)
+        return auditRepository.getAuditLogsByTaskId(taskId)
+    }
+
+    private fun validateTaskId(taskId: String) {
+        if (taskId.isBlank()) throw InvalidInputException(INVALID_ID_ERROR)
+    }
+
+    companion object {
+        private const val INVALID_ID_ERROR = "Error: ID shouldn't be blank"
     }
 }
