85a21e10e80b82f26ba32734431e95c2ccb8222e
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-04-30 15:23:46 +0300

    create update state fun use case for testing

--- src/main/kotlin/logic/useCases/state/UpdateStateUseCase.kt
+++ src/main/kotlin/logic/useCases/state/UpdateStateUseCase.kt
@@ -1,8 +1,12 @@
 package logic.useCases.state
 
+import logic.entities.State
 import logic.repository.StatesRepository
 
 class UpdateStateUseCase(
     private val statesRepository: StatesRepository
 ) {
+    fun updateState(state: State): Boolean{
+        return false
+    }
 }
