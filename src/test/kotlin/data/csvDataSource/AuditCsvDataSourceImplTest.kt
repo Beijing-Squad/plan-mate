503127b216e61d4e0d46d2b9a68b3c204f1baf22
Ahmed Almancy <ahmedalmancy20@gmail.com>
2025-04-30 20:44:38 +0300

    add test setup for the AuditCsvDataSourceImplTest

--- src/test/kotlin/data/csvDataSource/AuditCsvDataSourceImplTest.kt
+++ src/test/kotlin/data/csvDataSource/AuditCsvDataSourceImplTest.kt
@@ -1,5 +1,19 @@
 package data.csvDataSource
 
+import data.csvDataSource.csv.CsvDataSourceImpl
+import io.mockk.mockk
+import logic.entities.Audit
+import org.junit.jupiter.api.BeforeEach
+
 class AuditCsvDataSourceImplTest {
+
+    private lateinit var csvDataSource: CsvDataSourceImpl<Audit>
+    private lateinit var auditDataSource: AuditCsvDataSourceImpl
+
+    @BeforeEach
+    fun setUp() {
+        csvDataSource = mockk(relaxed = true)
+        auditDataSource = AuditCsvDataSourceImpl(csvDataSource)
+    }
 
 }
