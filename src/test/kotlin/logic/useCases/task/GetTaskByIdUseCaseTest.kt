1d0147b0107e22623d043681a23288dcfd0b0e27
Radwa85 <radwa.mohamed5033@gmail.com>
2025-04-30 20:30:18 +0300

    write test cases of get task by id with coverage 100%

--- src/test/kotlin/logic/useCases/task/GetTaskByIdUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/task/GetTaskByIdUseCaseTest.kt
@@ -1,7 +1,67 @@
 package logic.useCases.task
 
-import org.junit.jupiter.api.Assertions.*
+import com.google.common.truth.Truth.assertThat
+import fake.createTask
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.datetime.LocalDate
+import logic.entities.Task
+import logic.entities.exceptions.TaskNotFoundException
+import logic.repository.TasksRepository
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFailsWith
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalUuidApi::class)
 class GetTaskByIdUseCaseTest {
 
+    private lateinit var tasksRepository: TasksRepository
+    private lateinit var getTaskByIdUseCase: GetTaskByIdUseCase
+
+    @BeforeEach
+    fun setUp() {
+        tasksRepository = mockk(relaxed = true)
+        getTaskByIdUseCase = GetTaskByIdUseCase(tasksRepository)
+    }
+
+    @Test
+    fun `should return task when task id exists`() {
+        // Given
+        val task = createTask(
+            projectId = "project-1",
+            title = "Test Task",
+            description = "Test Description",
+            createdBy = "user-1",
+            stateId = "state-1",
+            createdAt = LocalDate(2023, 1, 1),
+            updatedAt = LocalDate(2023, 1, 1)
+        )
+        every { tasksRepository.getTaskById(task.id.toString()) } returns task
+
+        // When
+        val result = getTaskByIdUseCase.getTaskById(task.id.toString())
+
+        // Then
+        assertThat(result).isEqualTo(task)
+        verify { tasksRepository.getTaskById(task.id.toString()) }
+    }
+
+    @Test
+    fun `should throw TaskNotFoundException when task id does not exist`() {
+        // Given
+        val taskId = "no"
+        every { tasksRepository.getTaskById(taskId) } returns null
+
+        // When/Then
+        val exception = assertFailsWith<TaskNotFoundException> {
+            getTaskByIdUseCase.getTaskById(taskId)
+        }
+
+        // Then
+        assertThat(exception.message).isEqualTo("Task with ID $taskId not found")
+        verify { tasksRepository.getTaskById(taskId) }
+    }
 }
