9d6e1e75850f27a59657b3aca2e79b49c7b3c031
karrar-abbas <karar.abbas.ch@gmail.com>
2025-04-30 20:40:16 +0300

    implement get all projects use case and create test cases

--- src/main/kotlin/logic/useCases/project/GetAllProjectsUseCase.kt
+++ src/main/kotlin/logic/useCases/project/GetAllProjectsUseCase.kt
@@ -1,8 +1,18 @@
 package logic.useCases.project
 
+import logic.entities.Project
+import logic.entities.UserRole
+import logic.entities.exceptions.ProjectUnauthorizedUserException
 import logic.repository.ProjectsRepository
 
 class GetAllProjectsUseCase(
     private val projectsRepository: ProjectsRepository
 ) {
+    fun getAllProjects(role: UserRole): Result<List<Project>> {
+        return if (role != UserRole.ADMIN) {
+            Result.failure(ProjectUnauthorizedUserException("User Not Authorized"))
+        } else {
+            Result.success(projectsRepository.getAllProjects())
+        }
+    }
 }
