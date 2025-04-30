71c1350c96ee38319ea2c69d3285820860811488
mohammed-akkad <momoo362514@gmail.com>
2025-04-30 18:36:08 +0300

    complete plan mate console  ui test

---
+++ src/test/kotlin/ui/main/PlanMateConsoleUiTest.kt
@@ -0,0 +1,121 @@
+package ui.main
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class PlanMateConsoleUiTest {
+    private lateinit var planMateUi: PlanMateUi
+    private lateinit var consoleIO: ConsoleIO
+    private lateinit var screens: List<BaseScreen>
+
+    @BeforeEach
+    fun setup() {
+        consoleIO = mockk(relaxed = true)
+        screens = listOf(
+            mockk<BaseScreen>().apply {
+                every { id } returns "1"
+                every { name } returns "Suggestion Meal 🤔"
+                every { execute() } returns Unit
+            },
+            mockk<BaseScreen>().apply {
+                every { id } returns "2"
+                every { name } returns "Search Meal 🔎"
+                every { execute() } returns Unit
+            }
+        )
+        planMateUi = PlanMateConsoleUi(
+            screens,
+            consoleIO
+        )
+
+    }
+
+    @Test
+    fun `should show message welcome when start app`() {
+        // Given
+        every { consoleIO.read() } returns EXIT_INPUT
+
+        // When
+        planMateUi.start()
+
+        // When && Then
+        verify { consoleIO.show(any()) }
+    }
+
+    @Test
+    fun `should present feature when choice is one`() {
+        // Given
+        every { consoleIO.read() } returns INPUT_CHOICE andThen EXIT_INPUT
+
+        // When
+        planMateUi.start()
+
+        // Then
+        verify { consoleIO.showWithLine(any()) }
+    }
+
+    @Test
+    fun `should view message when write invalid option`() {
+        val firstChoice = "1a2b"
+        val secondChoice = " -@"
+        // Given
+        every { consoleIO.read() } returnsMany listOf(firstChoice, secondChoice, EXIT_INPUT)
+
+        // When
+        planMateUi.start()
+
+        // Then
+        verify {
+            consoleIO.showWithLine(any())
+        }
+    }
+
+    @Test
+    fun `should exit when choice zero from menu`() {
+        // Given
+        every { consoleIO.read() } returns EXIT_INPUT
+
+        // When
+        planMateUi.start()
+
+        // Then
+        verify { consoleIO.showWithLine(any()) }
+
+    }
+
+    @Test
+    fun `should handle exception when an unknown error occurred`() {
+        // Given
+        every { consoleIO.read() } throws Exception("An unknown error occurred")
+
+        // When
+        planMateUi.start()
+
+        // Then
+        verify { consoleIO.showWithLine("An unknown error occurred") }
+    }
+
+    @Test
+    fun `should execute feature when valid option is selected`() {
+        // Given
+        every { consoleIO.read() } returns INPUT_CHOICE andThen EXIT_INPUT
+
+        // When
+        planMateUi.start()
+
+        // Then
+        verify {
+            screens[0].execute()
+        }
+    }
+
+
+    private companion object {
+        const val EXIT_INPUT = "0"
+        const val INPUT_CHOICE = "1"
+    }
+
+}
