f84697543d69eb83d5a0ed0fc4e92e849bbc17e6
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-04-30 15:23:17 +0300

    create delete state fun use case for testing

--- src/main/kotlin/logic/useCases/state/DeleteStateUseCase.kt
+++ src/main/kotlin/logic/useCases/state/DeleteStateUseCase.kt
@@ -1,8 +1,12 @@
 package logic.useCases.state
 
+import logic.entities.State
 import logic.repository.StatesRepository
 
 class DeleteStateUseCase(
     private val statesRepository: StatesRepository
 ) {
+    fun deleteState(state: State): Boolean {
+        return false
+    }
 }
