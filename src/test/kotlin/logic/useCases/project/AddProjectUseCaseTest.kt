ebfe5b8a72332bc90852158fa0b0addfa90a29d3
karrar-abbas <karar.abbas.ch@gmail.com>
2025-04-30 20:38:49 +0300

    implement add project use case and create test cases

--- src/test/kotlin/logic/useCases/project/AddProjectUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/project/AddProjectUseCaseTest.kt
@@ -1,7 +1,68 @@
 package logic.useCases.project
 
-import org.junit.jupiter.api.Assertions.*
+import com.google.common.truth.Truth.assertThat
+import fake.createProject
+import io.mockk.every
+import io.mockk.mockk
+import logic.entities.UserRole
+import logic.entities.exceptions.ProjectAlreadyExistsException
+import logic.entities.exceptions.ProjectNameIsEmptyException
+import logic.entities.exceptions.ProjectUnauthorizedUserException
+import logic.repository.ProjectsRepository
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class AddProjectUseCaseTest {
+
+    private lateinit var addProject: AddProjectUseCase
+    private lateinit var projectRepository: ProjectsRepository
+
+
+    @BeforeEach
+    fun setUp() {
+        projectRepository = mockk(relaxed = true)
+        addProject = AddProjectUseCase(projectRepository)
+    }
+
+    @Test
+    fun `should create project when project is valid`() {
+        // Given
+        val project = createProject()
+        // When
+        val result = addProject.addProject(project,UserRole.ADMIN).getOrThrow()
+        // Then
+        assertThat(result).isEqualTo(true)
+    }
+
+    @Test
+    fun `should throw exception when project name is empty`() {
+        // Given
+        val project = createProject(name = "")
+        // When && Then
+        assertThrows<ProjectNameIsEmptyException> { addProject.addProject(project,UserRole.ADMIN).getOrThrow() }
+    }
+
+    @Test
+    fun `should throw exception when project is duplicate`() {
+        // Given
+        val project = createProject()
+        every { projectRepository.getAllProjects() } returns listOf(project)
+
+        // When && Then
+        assertThrows<ProjectAlreadyExistsException> { addProject.addProject(project,UserRole.ADMIN).getOrThrow() }
+
+    }
+
+    @Test
+    fun `should throw exception when user is not admin`() {
+        // Given
+        val project = createProject()
+
+        // When && Then
+        assertThrows<ProjectUnauthorizedUserException> { addProject.addProject(project,UserRole.MATE).getOrThrow() }
+
+    }
+
 
 }
