a4bc2ee42d9da1aff288d748111f5e06d88d845f
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-04-30 15:23:32 +0300

    create get all states fun use case for testing

--- src/main/kotlin/logic/useCases/state/GetAllStatesUseCase.kt
+++ src/main/kotlin/logic/useCases/state/GetAllStatesUseCase.kt
@@ -1,8 +1,12 @@
 package logic.useCases.state
 
+import logic.entities.State
 import logic.repository.StatesRepository
 
 class GetAllStatesUseCase(
     private val statesRepository: StatesRepository
 ) {
+    fun getAllStates(): List<State>{
+        return statesRepository.getAllStates()
+    }
 }
