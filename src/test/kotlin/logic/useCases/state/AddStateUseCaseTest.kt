4fce24ccadd66a1c2c511a4fbb83dbd9a3dca061
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-04-30 15:25:11 +0300

    add test case for add state use case

--- src/test/kotlin/logic/useCases/state/AddStateUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/state/AddStateUseCaseTest.kt
@@ -1,7 +1,43 @@
 package logic.useCases.state
 
-import org.junit.jupiter.api.Assertions.*
+import com.google.common.truth.Truth.assertThat
+import fake.createProject
+import fake.createState
+import io.mockk.mockk
+import logic.repository.StatesRepository
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+import kotlin.uuid.ExperimentalUuidApi
 
 class AddStateUseCaseTest {
+    private lateinit var statesRepository: StatesRepository
+    private lateinit var addStateUseCase: AddStateUseCase
+
+    @BeforeEach
+    fun setup() {
+        statesRepository = mockk()
+        addStateUseCase = AddStateUseCase(statesRepository)
+    }
+
+    @OptIn(ExperimentalUuidApi::class)
+    @Test
+    fun `add new state test`() {
+        //Given
+        val project = createProject(
+            name = "PlanMate Core Features",
+            createdBy = "adminUser01"
+        )
+        val newState = createState(
+            id = "123",
+            name = "Done",
+            projectId = project.id.toString()
+        )
+
+        // when
+        val result = addStateUseCase.addState(newState)
+
+        //Then
+        assertThat(result).isFalse()
+    }
 
 }
