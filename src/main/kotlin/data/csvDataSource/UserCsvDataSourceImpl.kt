ac49d33410ac739a7839d656540eed42b60c29bd
mohammed-akkad <momoo362514@gmail.com>
2025-04-30 16:11:47 +0300

    complete use csv data source implementation

--- src/main/kotlin/data/csvDataSource/UserCsvDataSourceImpl.kt
+++ src/main/kotlin/data/csvDataSource/UserCsvDataSourceImpl.kt
@@ -3,25 +3,34 @@ package data.csvDataSource
 import data.csvDataSource.csv.CsvDataSourceImpl
 import data.repository.dataSource.UserDataSource
 import logic.entities.User
+import logic.entities.exceptions.UserNotFoundException
+import kotlin.uuid.ExperimentalUuidApi
 
 class UserCsvDataSourceImpl(
     private val csvDataSource: CsvDataSourceImpl<User>
-): UserDataSource {
+) : UserDataSource {
+    private val users = csvDataSource.loadAll().toMutableList()
 
     override fun getAllUsers(): List<User> {
-        TODO("Not yet implemented")
+        return users.toList()
     }
 
+    @OptIn(ExperimentalUuidApi::class)
     override fun getUserByUserId(userId: String): User {
-        TODO("Not yet implemented")
+        return getAllUsers()
+            .find { it.id.toString() == userId }
+            ?: throw UserNotFoundException("user not found")
+
     }
 
-    override fun addUser(user: User) {
-        TODO("Not yet implemented")
-    }
-
+    @OptIn(ExperimentalUuidApi::class)
     override fun updateUser(user: User): User {
-        TODO("Not yet implemented")
+        val currentUser = getUserByUserId(user.id.toString())
+        val userUpdated = currentUser
+            .copy(userName = user.userName, password = user.password)
+        users[users.indexOf(currentUser)] = userUpdated
+        csvDataSource.update(users)
+        return currentUser
     }
 
 }
