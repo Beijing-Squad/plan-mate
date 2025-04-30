1b844ed5c67c32fffb49f1d2284321fc44dd2378
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-04-30 19:33:02 +0300

    change id state type from string to uuid

--- src/main/kotlin/logic/entities/State.kt
+++ src/main/kotlin/logic/entities/State.kt
@@ -1,7 +1,10 @@
 package logic.entities
 
-data class State(
-    val id: String,
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+data class State @OptIn(ExperimentalUuidApi::class) constructor(
+    val id: Uuid = Uuid.random(),
     val name: String,
     val projectId: String
 )
