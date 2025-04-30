c10c7634efd698d1824003dc63d92b3623bd95c0
mohammed-akkad <momoo362514@gmail.com>
2025-04-30 12:18:23 +0300

    update md5 password encryption test

--- src/test/kotlin/logic/useCases/user/cryptography/MD5PasswordEncryptionTest.kt
+++ src/test/kotlin/logic/useCases/user/cryptography/MD5PasswordEncryptionTest.kt
@@ -1,7 +1,6 @@
 package logic.useCases.user.cryptography
 
 import com.google.common.truth.Truth.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
