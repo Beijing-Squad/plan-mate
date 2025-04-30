e42f6158822ab6cc1328298d867de767356f4523
mohammed-akkad <momoo362514@gmail.com>
2025-04-30 12:17:51 +0300

    update get user by user id use case

--- src/main/kotlin/logic/useCases/user/GetUserByUserIdUseCase.kt
+++ src/main/kotlin/logic/useCases/user/GetUserByUserIdUseCase.kt
@@ -1,7 +1,6 @@
 package logic.useCases.user
 
 import logic.entities.User
-import logic.entities.exceptions.UserNotFoundException
 import logic.repository.UserRepository
 import kotlin.uuid.ExperimentalUuidApi
 
