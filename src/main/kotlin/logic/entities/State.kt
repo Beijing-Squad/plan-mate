65026873406f803620e8220fd87018d955b4e5fc
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-02 02:10:52 +0300

    roll back the state id type in state class

--- src/main/kotlin/logic/entities/State.kt
+++ src/main/kotlin/logic/entities/State.kt
@@ -1,10 +1,9 @@
 package logic.entities
 
 import kotlin.uuid.ExperimentalUuidApi
-import kotlin.uuid.Uuid
 
 data class State @OptIn(ExperimentalUuidApi::class) constructor(
-    val id: Uuid = Uuid.random(),
+    val id: String,
     val name: String,
     val projectId: String
 )
