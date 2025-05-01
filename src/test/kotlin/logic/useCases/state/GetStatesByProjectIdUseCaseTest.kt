a1b13646bd3dfada63413c243b6611bb1d3addde
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-01 14:45:04 +0300

    add test use case for get state by project id

--- src/test/kotlin/logic/useCases/state/GetStatesByProjectIdUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/state/GetStatesByProjectIdUseCaseTest.kt
@@ -4,4 +4,37 @@ import org.junit.jupiter.api.Assertions.*
 
 class GetStatesByProjectIdUseCaseTest {
 
+    private lateinit var getStatesByProjectIdUseCase: GetStatesByProjectIdUseCase
+    private lateinit var stateRepository: StatesRepository
+
+
+    @BeforeEach
+    fun setUp() {
+        stateRepository = mockk(relaxed = true)
+        getStatesByProjectIdUseCase = GetStatesByProjectIdUseCase(stateRepository)
+    }
+
+
+    @OptIn(ExperimentalUuidApi::class)
+    @Test
+    fun `should return states of project when are exist`() {
+        // Given
+        val project = createProject()
+        val projectId = project.id.toString()
+        val states = listOf(
+            createState(
+                projectId = projectId
+            ),
+            createState(
+                projectId = project.id.toString()
+            )
+        )
+        every { stateRepository.getStatesByProjectId(projectId) } returns states
+        // When
+        val result = getStatesByProjectIdUseCase.getStatesByProjectId(projectId)
+        // Then
+        result.forEach { state ->
+            assertThat(state.projectId).isEqualTo(projectId)
+        }
+    }
 }
