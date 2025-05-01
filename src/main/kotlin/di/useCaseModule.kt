91bef22e78ec2fcd4084b98ac0527ee5a54aa869
Radwa85 <radwa.mohamed5033@gmail.com>
2025-05-01 19:35:18 +0300

    added uses cases modules

--- src/main/kotlin/di/useCaseModule.kt
+++ src/main/kotlin/di/useCaseModule.kt
@@ -1,9 +1,16 @@
 package di
 
+import logic.useCases.task.GetAllTasksUseCase
+import logic.useCases.task.GetTaskByIdUseCase
+import logic.useCases.task.UpdateTaskUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
 
     // Add use cases here
+
+    single { GetAllTasksUseCase(get()) }
+    single { GetTaskByIdUseCase(get()) }
+    single { UpdateTaskUseCase(get()) }
 
 }
