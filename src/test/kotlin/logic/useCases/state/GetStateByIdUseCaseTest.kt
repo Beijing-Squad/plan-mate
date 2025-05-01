c7c45fa039ac8396d40172f7f15a2a1564ed80f8
Mohamed Elsadany <mohamedali802060@gamil.com>
2025-05-01 10:06:19 +0300

    create test cases for get state by id use case

--- src/test/kotlin/logic/useCases/state/GetStateByIdUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/state/GetStateByIdUseCaseTest.kt
@@ -1,7 +1,47 @@
 package logic.useCases.state
 
-import org.junit.jupiter.api.Assertions.*
+import com.google.common.truth.Truth.assertThat
+import fake.createState
+import io.mockk.every
+import io.mockk.mockk
+import logic.repository.StatesRepository
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
 
 class GetStateByIdUseCaseTest {
+    private lateinit var statesRepository: StatesRepository
+    private lateinit var getStateByIdUseCase: GetStateByIdUseCase
+
+    @BeforeEach
+    fun setup() {
+        statesRepository = mockk()
+        getStateByIdUseCase = GetStateByIdUseCase(statesRepository)
+    }
+
+    @Test
+    fun shouldReturnStateWhenIdExists() {
+        // Given
+        val expectedState = createState(id = "456", name = "Review", projectId = "project-2")
+
+        every { statesRepository.getStateById("456") } returns expectedState
+
+        // When
+        val result = getStateByIdUseCase.getStateById(stateId = "456")
+
+        // Then
+        assertThat(result).isEqualTo(expectedState)
+    }
+
+    @Test
+    fun shouldReturnNullWhenStateIdDoesNotExist() {
+        // Given
+        every { statesRepository.getStateById("999") } returns null
+
+        // When
+        val result = getStateByIdUseCase.getStateById("999")
+
+        // Then
+        assertThat(result).isNull()
+    }
 
 }
