45a966d0113e11ca858a3fec8b333da0d6dc44d1
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-02 01:19:55 +0300

    add user role check for add state use case

--- src/main/kotlin/logic/useCases/state/AddStateUseCase.kt
+++ src/main/kotlin/logic/useCases/state/AddStateUseCase.kt
@@ -1,7 +1,9 @@
 package logic.useCases.state
 
 import logic.entities.State
+import logic.entities.UserRole
 import logic.entities.exceptions.StateAlreadyExistException
+import logic.entities.exceptions.StateUnauthorizedUserException
 import logic.repository.StatesRepository
 import kotlin.uuid.ExperimentalUuidApi
 
@@ -9,7 +11,10 @@ class AddStateUseCase(
     private val statesRepository: StatesRepository
 ) {
     @OptIn(ExperimentalUuidApi::class)
-    fun addState(state: State): Boolean {
+    fun addState(state: State, role: UserRole): Boolean {
+        if (role != UserRole.ADMIN){
+            throw StateUnauthorizedUserException("user should be Admin")
+        }
         val allStats = statesRepository.getAllStates()
         if (allStats.any { it.id == state.id }) {
             throw StateAlreadyExistException("State with id ${state.id} already exists")
