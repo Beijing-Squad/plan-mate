8f09b2165f3c671bebd296e5782803f215bbb4d4
Mohamed Elsadany <mohamedali802060@gamil.com>
2025-05-01 10:04:04 +0300

    create get state by id fun use case for testing

--- src/main/kotlin/logic/useCases/state/GetStateByIdUseCase.kt
+++ src/main/kotlin/logic/useCases/state/GetStateByIdUseCase.kt
@@ -1,8 +1,12 @@
 package logic.useCases.state
 
+import logic.entities.State
 import logic.repository.StatesRepository
 
 class GetStateByIdUseCase(
     private val statesRepository: StatesRepository
 ) {
+    fun getStateById(stateId: String): State? {
+        return statesRepository.getStateById(stateId)
+    }
 }
