bddfcbf3d65479d960b4cdf831a1f67343a7312e
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-02 01:21:08 +0300

    add get state by project id use case impl

--- src/main/kotlin/logic/useCases/state/GetStatesByProjectIdUseCase.kt
+++ src/main/kotlin/logic/useCases/state/GetStatesByProjectIdUseCase.kt
@@ -1,6 +1,7 @@
 package logic.useCases.state
 
 import logic.entities.State
+import logic.entities.exceptions.StateNotFoundException
 import logic.repository.StatesRepository
 
 class GetStatesByProjectIdUseCase(
@@ -8,5 +9,10 @@ class GetStatesByProjectIdUseCase(
 ) {
     fun getStatesByProjectId(projectId: String): List<State> {
         return statesRepository.getStatesByProjectId(projectId)
+            .also {
+                if (it.isEmpty()) {
+                    throw StateNotFoundException("the state with this $projectId project id not found")
+                }
+            }
     }
 }
