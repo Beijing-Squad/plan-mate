1ccc4ac82a855d7230181a5077d1aa3cba008e43
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-01 14:01:11 +0300

    create get states by project id use case for testing

--- src/main/kotlin/logic/useCases/state/GetStatesByProjectIdUseCase.kt
+++ src/main/kotlin/logic/useCases/state/GetStatesByProjectIdUseCase.kt
@@ -1,8 +1,12 @@
 package logic.useCases.state
 
+import logic.entities.State
 import logic.repository.StatesRepository
 
 class GetStatesByProjectIdUseCase(
     private val statesRepository: StatesRepository
 ) {
+    fun getStatesByProjectId(projectId: String): List<State> {
+        return statesRepository.getStatesByProjectId(projectId)
+    }
 }
