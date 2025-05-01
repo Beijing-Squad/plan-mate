53ee819eac4479c62aa23b5ccbf57a64e9ef403d
mohammed-akkad <momoo362514@gmail.com>
2025-05-01 14:15:46 +0300

    update use case module

--- src/main/kotlin/di/useCaseModule.kt
+++ src/main/kotlin/di/useCaseModule.kt
@@ -1,9 +1,24 @@
 package di
 
+import logic.useCases.user.GetAllUsersUseCase
+import logic.useCases.user.GetUserByUserIdUseCase
+import logic.useCases.user.UpdateUserUseCase
+import logic.useCases.user.ValidateUserUseCase
+import logic.useCases.user.cryptography.MD5PasswordEncryption
+import logic.useCases.user.cryptography.PasswordEncryption
 import org.koin.dsl.module
 
 val useCaseModule = module {
 
     // Add use cases here
+
+
+
+
+    single { GetAllUsersUseCase(get()) }
+    single { GetUserByUserIdUseCase(get()) }
+    single<PasswordEncryption> { MD5PasswordEncryption() }
+    single { ValidateUserUseCase() }
+    single { UpdateUserUseCase(get(),get(),get()) }
 
 }
