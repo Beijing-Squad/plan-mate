c1db3dfa6c6de017bd0b92c6c84cea3fb8f07399
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-01 04:47:43 +0300

    refactor the update state test case

--- src/test/kotlin/logic/useCases/state/UpdateStateUseCaseTest.kt
+++ src/test/kotlin/logic/useCases/state/UpdateStateUseCaseTest.kt
@@ -1,12 +1,13 @@
 package logic.useCases.state
 
+import com.google.common.truth.Truth.assertThat
 import fake.createProject
 import fake.createState
+import io.mockk.every
 import io.mockk.mockk
 import logic.repository.StatesRepository
 import org.junit.jupiter.api.BeforeEach
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
 import kotlin.uuid.ExperimentalUuidApi
 
 class UpdateStateUseCaseTest {
@@ -21,23 +22,29 @@ class UpdateStateUseCaseTest {
 
     @OptIn(ExperimentalUuidApi::class)
     @Test
-    fun `update new state test`() {
+    fun `should update state when state is exist`() {
         //Given
         val project = createProject(
             name = "PlanMate Core Features",
             createdBy = "adminUser01"
         )
-        val newState = createState(
-            name = "Done",
+        val state = createState(
+            name = "in progress",
             projectId = project.id.toString()
         )
+        val states = listOf(state, state)
+        val newState = createState(
+            id = state.id,
+            name = "done",
+            projectId = state.projectId
+        )
+
+        every { statesRepository.updateState(newState) } returns true
 
         // when
-//        every { statesRepository.updateState(newState) } returns true
         val result = updateStateUseCase.updateState(newState)
 
         //Then
-        assertEquals(true, result)
+        assertThat(result).isTrue()
     }
-
 }
