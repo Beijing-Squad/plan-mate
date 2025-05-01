392bcf7a583b2531e243771fa59913c88840b5db
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-01 18:54:46 +0300

    add implementation of add state use case

--- src/main/kotlin/logic/useCases/state/AddStateUseCase.kt
+++ src/main/kotlin/logic/useCases/state/AddStateUseCase.kt
@@ -1,12 +1,19 @@
 package logic.useCases.state
 
 import logic.entities.State
+import logic.entities.exceptions.StateAlreadyExistException
 import logic.repository.StatesRepository
+import kotlin.uuid.ExperimentalUuidApi
 
 class AddStateUseCase(
     private val statesRepository: StatesRepository
 ) {
+    @OptIn(ExperimentalUuidApi::class)
     fun addState(state: State): Boolean {
+        val allStats = statesRepository.getAllStates()
+        if (allStats.any { it.id == state.id }) {
+            throw StateAlreadyExistException("State with id ${state.id} already exists")
+        }
         return statesRepository.addState(state)
     }
 }
