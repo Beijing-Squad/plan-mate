69b5803bbeb04b476cce5561716325f9fbaaae07
Mohamed <m.3.abdelfadeel@gmail.com>
2025-05-02 14:43:34 +0300

    refactor md5

--- src/main/kotlin/logic/useCases/authentication/RegisterUserAuthenticationUseCase.kt
+++ src/main/kotlin/logic/useCases/authentication/RegisterUserAuthenticationUseCase.kt
@@ -5,18 +5,18 @@ import logic.entities.UserRole
 import logic.entities.exceptions.UserExistsException
 import logic.repository.AuthenticationRepository
 import logic.repository.UserRepository
-import logic.useCases.hashPassword
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 class RegisterUserAuthenticationUseCase(
     private val repository: AuthenticationRepository,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val md5Password: MD5PasswordUseCase
 ) {
     @OptIn(ExperimentalUuidApi::class)
     fun execute(username: String, password: String, role: UserRole): Boolean {
         require(username.isNotBlank()) { "Username cannot be blank" }
-        val hashedPassword = hashPassword(password)
+        val hashedPassword = md5Password.hashPassword(password)
         val user = User(Uuid.random(), username, hashedPassword, role)
         val existingUser = userRepository.getAllUsers().find { it.userName == username }
 
