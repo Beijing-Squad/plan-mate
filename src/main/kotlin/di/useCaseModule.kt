8833a70a52f4b4d6d9f17e56d0c7b0205d8e7f6e
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-02 17:50:34 +0300

    add state use cases to use case module

--- src/main/kotlin/di/useCaseModule.kt
+++ src/main/kotlin/di/useCaseModule.kt
@@ -1,9 +1,21 @@
 package di
 
+import logic.useCases.state.AddStateUseCase
+import logic.useCases.state.DeleteStateUseCase
+import logic.useCases.state.GetAllStatesUseCase
+import logic.useCases.state.GetStateByIdUseCase
+import logic.useCases.state.GetStatesByProjectIdUseCase
+import logic.useCases.state.UpdateStateUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
 
     // Add use cases here
+    single<AddStateUseCase> { AddStateUseCase(get()) }
+    single<DeleteStateUseCase> { DeleteStateUseCase(get()) }
+    single<GetStateByIdUseCase> { GetStateByIdUseCase(get()) }
+    single<GetStatesByProjectIdUseCase> { GetStatesByProjectIdUseCase(get()) }
+    single<GetAllStatesUseCase> { GetAllStatesUseCase(get()) }
+    single< UpdateStateUseCase> { UpdateStateUseCase(get()) }
 
 }
