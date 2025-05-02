298d3c650f5d5a88d19330864801fc7091f12912
karrar-abbas <karar.abbas.ch@gmail.com>
2025-05-02 16:45:27 +0300

    add project useCases to useCaseModule

--- src/main/kotlin/di/useCaseModule.kt
+++ src/main/kotlin/di/useCaseModule.kt
@@ -1,9 +1,13 @@
 package di
 
+import logic.useCases.project.*
 import org.koin.dsl.module
 
 val useCaseModule = module {
-
     // Add use cases here
-
+    single { AddProjectUseCase(get()) }
+    single { DeleteProjectUseCase(get()) }
+    single { GetAllProjectsUseCase(get()) }
+    single { GetProjectByIdUseCase(get()) }
+    single { UpdateProjectUseCase(get()) }
 }
