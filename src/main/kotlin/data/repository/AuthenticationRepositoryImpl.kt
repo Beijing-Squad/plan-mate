8b62247132f84e03f39962e747aff82c0f65546c
Mohamed <m.3.abdelfadeel@gmail.com>
2025-05-05 17:46:46 +0300

    refactor validation and hash password from logic to data source impl

--- src/main/kotlin/data/repository/AuthenticationRepositoryImpl.kt
+++ src/main/kotlin/data/repository/AuthenticationRepositoryImpl.kt
@@ -2,13 +2,16 @@ package data.repository
 
 import data.repository.dataSource.AuthenticationDataSource
 import logic.entities.User
+import logic.entities.UserRole
 import logic.repository.AuthenticationRepository
 
 class AuthenticationRepositoryImpl(
     private val dataSource: AuthenticationDataSource
 ) : AuthenticationRepository {
 
-    override fun registerUser(user: User): Boolean = dataSource.saveUser(user)
-    override fun loginUser(username: String, password: String): User = dataSource.getUserByUsername(username)
+    override fun registerUser(username: String, password: String, role: UserRole): Boolean =
+        dataSource.saveUser(username, password, role)
 
+    override fun loginUser(username: String, password: String): User =
+        dataSource.getAuthenticatedUser(username, password)
 }
