8aed47e02d048060fd0c34955a892879435c8da9
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-06 01:45:08 +0300

    make state entity take default it

--- src/main/kotlin/logic/entities/State.kt
+++ src/main/kotlin/logic/entities/State.kt
@@ -1,9 +1,10 @@
 package logic.entities
 
 import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 data class State @OptIn(ExperimentalUuidApi::class) constructor(
-    val id: String,
+    val id: String = Uuid.random().toString(),
     val name: String,
     val projectId: String
 )
