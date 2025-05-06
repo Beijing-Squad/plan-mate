9b41f0f117c91cd8544608956723456096867191
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-06 16:10:13 +0300

    change id in fake create state in test

--- src/test/kotlin/fake/createState.kt
+++ src/test/kotlin/fake/createState.kt
@@ -1,14 +1,17 @@
 package fake
 
 import logic.entities.TaskState
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalUuidApi::class)
 fun createState(
     id: String = "DefaultStateId",
     name: String = "defaultStateName",
     projectId: String = "defaultProjectId"
 ): TaskState {
     return TaskState(
-        id = id,
+        id = Uuid.parse(id),
         name = name,
         projectId = projectId
     )
