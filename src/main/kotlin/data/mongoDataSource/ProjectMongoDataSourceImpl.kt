234a5aa007da93707ad98dc9eb5c1688290ba8ab
Radwa85 <radwa.mohamed5033@gmail.com>
2025-05-06 15:58:54 +0300

    implementation of ProjectDataSource using mongo db as the data source.

---
+++ src/main/kotlin/data/mongoDataSource/ProjectMongoDataSourceImpl.kt
@@ -0,0 +1,35 @@
+package data.mongoDataSource
+
+import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import data.mongoDataSource.mongoConnection.MongoConnection
+import data.repository.dataSource.ProjectDataSource
+import kotlinx.coroutines.CoroutineScope
+import logic.entities.Project
+
+class ProjectMongoDataSourceImpl(
+    private val database: MongoDatabase = MongoConnection.database,
+    private val dbScope: CoroutineScope = MongoConnection.dbScope
+) : ProjectDataSource {
+    private val collection = database.getCollection<Project>("projects")
+
+
+    override fun getAllProjects(): List<Project> {
+        TODO("Not yet implemented")
+    }
+
+    override fun addProject(project: Project) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteProject(projectId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateProject(newProjects: Project) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getProjectById(projectId: String): Project {
+        TODO("Not yet implemented")
+    }
+}
