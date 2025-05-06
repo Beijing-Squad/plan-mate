0e3a3590c7da8210db0704cc80e4fd105b20001d
Radwa85 <radwa.mohamed5033@gmail.com>
2025-05-06 17:27:47 +0300

    removed extra spaces

--- src/main/kotlin/data/repository/TasksRepositoryImpl.kt
+++ src/main/kotlin/data/repository/TasksRepositoryImpl.kt
@@ -7,21 +7,15 @@ class TasksRepositoryImpl(
     private val tasksDataSource: TasksDataSource
 ) : TasksRepository {
 
-    override fun getAllTasks(): List<Task> {
-        return tasksDataSource.getAllTasks()
-    }
+    override fun getAllTasks(): List<Task> = tasksDataSource.getAllTasks()
 
-    override fun getTaskById(taskId: String): Task {
-        return tasksDataSource.getTaskById(taskId)
-
-    }
+    override fun getTaskById(taskId: String): Task = tasksDataSource.getTaskById(taskId)
 
     override fun addTask(task: Task) = tasksDataSource.addTask(task)
 
     override fun deleteTask(taskId: String) = tasksDataSource.deleteTask(taskId)
 
-    override fun updateTask( updatedTask: Task): Task {
-        return tasksDataSource.updateTask(updatedTask)
+    override fun updateTask( updatedTask: Task) = tasksDataSource.updateTask(updatedTask)
 
-    }
+
 }
