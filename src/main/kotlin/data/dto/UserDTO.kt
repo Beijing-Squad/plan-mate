0e33005a70a85c13f4e11410a753ebe8b939a2d8
Mohamed <m.3.abdelfadeel@gmail.com>
2025-05-08 19:08:41 +0300

    update needed changes in userdto

--- src/main/kotlin/data/dto/UserDTO.kt
+++ src/main/kotlin/data/dto/UserDTO.kt
@@ -3,8 +3,8 @@ package data.dto
 import logic.entities.UserRole
 
 data class UserDTO(
-    val id: String? = null,
+    val id: String,
     val userName: String,
     val password: String,
-    val role: UserRole
+    val role: String
 )
