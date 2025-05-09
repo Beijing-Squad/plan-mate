4f4f72ce2a53b87f6578e01b0aa7a8103a252d08
Radwa85 <radwa.mohamed5033@gmail.com>
2025-05-09 21:32:09 +0300

    added to entity task

--- src/main/kotlin/logic/useCases/task/GetTaskByIdUseCase.kt
+++ src/main/kotlin/logic/useCases/task/GetTaskByIdUseCase.kt
@@ -1,12 +1,13 @@
 package logic.useCases.task
 
+import data.repository.mapper.toTaskEntity
 import logic.entities.Task
 import logic.repository.TasksRepository
 
 class GetTaskByIdUseCase(
     private val tasksRepository: TasksRepository
 ) {
-      fun getTaskById(taskId: String): Task {
-        return tasksRepository.getTaskById(taskId)
+      suspend fun getTaskById(taskId: String): Task {
+          toTaskEntity(return tasksRepository.getTaskById(taskId))
     }
 }
