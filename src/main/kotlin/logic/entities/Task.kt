5aeb1edd5e0ee4e1cb7f5efb1d0ae33e4d57bf04
Radwa85 <radwa.mohamed5033@gmail.com>
2025-05-09 14:37:33 +0300

    Add BsonId annotation to task id field

--- src/main/kotlin/logic/entities/Task.kt
+++ src/main/kotlin/logic/entities/Task.kt
@@ -1,11 +1,13 @@
 package logic.entities
 
 import kotlinx.datetime.LocalDateTime
+import org.bson.codecs.pojo.annotations.BsonId
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 @OptIn(ExperimentalUuidApi::class)
 data class Task(
+    @BsonId
     val id: Uuid = Uuid.random(),
     val projectId: String,
     val title: String,
