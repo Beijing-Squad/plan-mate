a4dd094f603c8ce9d737352073a387c16d8fb5ba
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-09 17:32:45 +0300

    88-refactor StatesRepository to use suspend functions

--- src/main/kotlin/logic/repository/StatesRepository.kt
+++ src/main/kotlin/logic/repository/StatesRepository.kt
@@ -3,16 +3,16 @@ package logic.repository
 import logic.entities.TaskState
 
 interface StatesRepository{
-    fun getAllStates(): List<TaskState>
+    suspend fun getAllStates(): List<TaskState>
 
-    fun getStatesByProjectId(projectId: String): List<TaskState>
+    suspend fun getStatesByProjectId(projectId: String): List<TaskState>
 
-    fun getStateById(stateId: String): TaskState
+    suspend fun getStateById(stateId: String): TaskState
 
-    fun addState(taskState: TaskState): Boolean
+    suspend fun addState(taskState: TaskState): Boolean
 
-    fun updateState(taskState: TaskState): TaskState
+    suspend fun updateState(taskState: TaskState): TaskState
 
-    fun deleteState(taskState: TaskState): Boolean
+    suspend fun deleteState(taskState: TaskState): Boolean
 
 }
