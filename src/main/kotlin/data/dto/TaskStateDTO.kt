584da3052529e005f7ecfacc7d2dd0b73791f066
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-10 23:00:45 +0300

    Revert "add Bson annotation for all Dtos"
    This reverts commit 9b7ccf3ac599b7ac87931d0a409b6cb1548df321.

--- src/main/kotlin/data/dto/TaskStateDTO.kt
+++ src/main/kotlin/data/dto/TaskStateDTO.kt
@@ -1,12 +1,9 @@
-package data.remote.datasource.dto
+package data.dto
 
-import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.codecs.pojo.annotations.BsonProperty
 
 data class TaskStateDTO(
-
-    @BsonId val id: String,
+    @BsonProperty("id") val id: String,
     @BsonProperty("name") val name: String,
     @BsonProperty("projectId") val projectId: String
-
 )
