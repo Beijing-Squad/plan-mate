b6f50968aa71532f758db1b277bb00b1ce8fbedc
Karim Al-Zmzmy <karam.deabes@gmail.com>
2025-05-10 20:56:01 +0300

    refactor local data sources into a unified interface for better modularity

---
+++ src/main/kotlin/data/repository/dataSource/LocalDataSource.kt
@@ -0,0 +1,38 @@
+package data.repository.dataSource
+
+import logic.entities.*
+
+interface LocalDataSource {
+
+    fun saveUser(username: String, password: String, role: UserRole): Boolean
+    fun getAuthenticatedUser(username: String, password: String): User
+
+    fun getAllAuditLogs(): List<Audit>
+    fun addAuditLog(audit: Audit)
+    fun getAuditLogsByProjectId(projectId: String): List<Audit>
+    fun getAuditLogsByTaskId(taskId: String): List<Audit>
+
+    fun getAllProjects(): List<Project>
+    fun addProject(project: Project)
+    fun deleteProject(projectId: String)
+    fun updateProject(newProjects: Project)
+    fun getProjectById(projectId: String): Project
+
+    fun getAllTasks(): List<Task>
+    fun getTaskById(taskId: String): Task
+    fun addTask(task: Task)
+    fun deleteTask(taskId: String)
+    fun updateTask(updatedTask: Task): Task
+
+    fun getAllStates(): List<TaskState>
+    fun getTaskStatesByProjectId(projectId: String): List<TaskState>
+    fun getTaskStateById(stateId: String): TaskState
+    fun addTaskState(taskState: TaskState): Boolean
+    fun updateTaskState(taskState: TaskState): TaskState
+    fun deleteTaskState(taskState: TaskState): Boolean
+
+
+    fun getAllUsers(): List<User>
+    fun getUserByUserId(userId: String): User
+    fun updateUser(user: User): User
+}
