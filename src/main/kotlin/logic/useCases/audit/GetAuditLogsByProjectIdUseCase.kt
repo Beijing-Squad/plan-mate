12af08b1d1d80284afecd8c0957c01683689999f
Mohamed Elsadany <mohamedali802060@gamil.com>
2025-05-12 20:30:19 +0300

    refactor get audit logs by project id use case

--- src/main/kotlin/logic/useCases/audit/GetAuditLogsByProjectIdUseCase.kt
+++ src/main/kotlin/logic/useCases/audit/GetAuditLogsByProjectIdUseCase.kt
@@ -1,28 +1,19 @@
-package logic.useCases.audit
 
 import logic.entities.Audit
-import logic.exceptions.InvalidInputException
-import logic.exceptions.ProjectNotFoundException
 import logic.repository.AuditRepository
 
 class GetAuditLogsByProjectIdUseCase(
     private val auditRepository: AuditRepository
 ) {
-    suspend fun getAuditLogsByProjectId(projectId: String): List<Audit> {
-        if (projectId.isBlank()) throw InvalidInputException(INVALID_ID_ERROR)
+    suspend fun getAuditLogsByProjectId(projectId: String): List<Audit>? {
+        if (!projectId.isValidProjectId()) return null
 
         return auditRepository.getAuditLogsByProjectId(projectId)
-            .orThrowIfEmpty(PROJECT_NOT_FOUND_ERROR)
-            .sortedByDescending { it.timeStamp }
+            .takeIf { it.isNotEmpty() }
+            ?.sortedByDescending { it.timeStamp }
     }
+}
 
-    private fun <T> Collection<T>.orThrowIfEmpty(errorMessage: String): Collection<T> {
-        if (isEmpty()) throw ProjectNotFoundException(errorMessage)
-        return this
-    }
-
-    companion object {
-        private const val INVALID_ID_ERROR = "Error: ID shouldn't be blank"
-        private const val PROJECT_NOT_FOUND_ERROR = "Error: Project logs not found"
-    }
+private fun String?.isValidProjectId(): Boolean {
+    return !this.isNullOrBlank()
 }
