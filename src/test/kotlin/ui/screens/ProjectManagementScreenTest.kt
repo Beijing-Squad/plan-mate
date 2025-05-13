8b076cc602ade7d5d2604249b65d7b3febe5cf1e
Zeinab979 <zeinabwahdan3677@gmail.com>
2025-05-13 10:58:09 +0300

    add test cases for delete and update project

--- src/test/kotlin/ui/screens/ProjectManagementScreenTest.kt
+++ src/test/kotlin/ui/screens/ProjectManagementScreenTest.kt
@@ -1,8 +1,10 @@
 package ui.screens
 
 import fake.createProject
+import fake.createUser
 import io.mockk.*
 import kotlinx.coroutines.test.runTest
+import logic.entities.Audit
 import logic.useCases.audit.AddAuditLogUseCase
 import logic.useCases.authentication.SessionManagerUseCase
 import logic.useCases.project.*
@@ -128,5 +130,71 @@ class ProjectManagementScreenTest {
         verify {
             consoleIO.showWithLine("\u001B[31m❌ Invalid Option\u001B[0m")
         }
+    }
+    @Test
+    fun `should update project when selecting option 3`() = runTest {
+        // Given
+        val existingProject = createProject()
+        val updatedName = "Updated Name"
+        val updatedDescription = "Updated Desc"
+        val id = existingProject.id.toString()
+
+        every { consoleIO.read() } returnsMany listOf("3", id, updatedName, updatedDescription, "0")
+        coEvery { getProjectByIdUseCase.getProjectById(id) } returns existingProject
+        every { sessionManager.getCurrentUser() } returns createUser()
+        // When
+        projectScreen.handleFeatureChoice()
+
+        // Then
+        coVerify {
+            getProjectByIdUseCase.getProjectById(id)
+            updateProjectUseCase.updateProject(match {
+                it.id == existingProject.id &&
+                        it.name == updatedName &&
+                        it.description == updatedDescription &&
+                        it.createdAt == existingProject.createdAt
+            })
+            addAuditLogUseCase.addAuditLog(match {
+                it.userName == "defaultUser" &&
+                        it.action == Audit.ActionType.UPDATE &&
+                        it.entityType == Audit.EntityType.PROJECT &&
+                        it.entityId == id
+            })
+        }
+
+        verify {
+            consoleIO.showWithLine("\u001B[32m✅ Project updated successfully.\u001B[0m")
+        }
+    }
+    @Test
+    fun `should delete project and log audit when selecting option 5`() = runTest {
+        // Given
+        val existingProject = createProject()
+        val projectId = existingProject.id.toString()
+        val mockUser = createUser()
+
+        every { consoleIO.read() } returnsMany listOf("5", projectId, "0")
+
+        coEvery { getProjectByIdUseCase.getProjectById(projectId) } returns existingProject
+
+        every { sessionManager.getCurrentUser() } returns mockUser
+
+        // When
+        projectScreen.handleFeatureChoice()
+
+        // Then
+        coVerify { deleteProjectUseCase.deleteProject(projectId) }
+        coVerify {
+            addAuditLogUseCase.addAuditLog(match {
+                it.entityType == Audit.EntityType.PROJECT &&
+                        it.action == Audit.ActionType.DELETE &&
+                        it.entityId == projectId &&
+                        it.userName == "defaultUser" &&
+                        it.userRole == mockUser.role &&
+                        it.actionDetails?.contains("deleted project with name") == true
+            })
+        }
+
+        verify { consoleIO.showWithLine("\u001B[32m✅ Project deleted successfully.\u001B[0m") }
     }
 }
