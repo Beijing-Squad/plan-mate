fa744253b6a88de9eeab7632b313bd57c9e6a4ab
Radwa85 <radwa.mohamed5033@gmail.com>
2025-05-14 18:14:05 +0300

    refactor variable names and add package declaration

--- src/main/kotlin/ui/main/FormatDateTime.kt
+++ src/main/kotlin/ui/main/FormatDateTime.kt
@@ -1,10 +1,12 @@
+package ui.main
+
 import kotlinx.datetime.LocalDateTime
 
 fun LocalDateTime.format(): String {
-    val monthString = monthNumber.toString().padStart(2, '0')
-    val dayString = dayOfMonth.toString().padStart(2, '0')
-    val hourString = hour.toString().padStart(2, '0')
-    val minuteString = minute.toString().padStart(2, '0')
+    val month = monthNumber.toString().padStart(2, '0')
+    val day = dayOfMonth.toString().padStart(2, '0')
+    val hour = hour.toString().padStart(2, '0')
+    val minute = minute.toString().padStart(2, '0')
 
-    return "$year-$monthString-$dayString,$hourString:$minuteString"
+    return "$year-$month-$day,$hour:$minute"
 }
